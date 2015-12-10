059d02fe0d550197b5c51055a24dec9881256a58
Theodore Ts'o <tytso@mit.edu>
2015-12-10 00:57:58 -0500

    UPSTREAM: ext4 crypto: add missing locking for keyring_key access
    commit db7730e3091a52c2fcd8fcc952b964d88998e675 upstream.
    
    Signed-off-by: Theodore Ts'o <tytso@mit.edu>
    Signed-off-by: Greg Kroah-Hartman <gregkh@linuxfoundation.org>
    (cherry picked from commit b80b70ef40459ec9e7493cef5e91e4e11dad9e42)
    Change-Id: I004fd40131905f5cebe0e830c5eeedc3a3c97ba7
    Signed-off-by: Eric Biggers <ebiggers@google.com>

--- fs/ext4/crypto_key.c
+++ fs/ext4/crypto_key.c
@@ -208,18 +208,41 @@ retry:
 		goto out;
 	}
 	crypt_info->ci_keyring_key = keyring_key;
+<<<<<<< HEAD
 	BUG_ON(keyring_key->type != &key_type_logon);
+=======
+	if (keyring_key->type != &key_type_logon) {
+		printk_once(KERN_WARNING
+			    "ext4: key type must be logon\n");
+		res = -ENOKEY;
+		goto out;
+	}
+	down_read(&keyring_key->sem);
+>>>>>>> fca9d9623b16... UPSTREAM: ext4 crypto: add missing locking for keyring_key access
 	ukp = ((struct user_key_payload *)keyring_key->payload.data);
 	if (ukp->datalen != sizeof(struct ext4_encryption_key)) {
 		res = -EINVAL;
+		up_read(&keyring_key->sem);
 		goto out;
 	}
 	master_key = (struct ext4_encryption_key *)ukp->data;
 	BUILD_BUG_ON(EXT4_AES_128_ECB_KEY_SIZE !=
 		     EXT4_KEY_DERIVATION_NONCE_SIZE);
+<<<<<<< HEAD
 	BUG_ON(master_key->size != EXT4_AES_256_XTS_KEY_SIZE);
+=======
+	if (master_key->size != EXT4_AES_256_XTS_KEY_SIZE) {
+		printk_once(KERN_WARNING
+			    "ext4: key size incorrect: %d\n",
+			    master_key->size);
+		res = -ENOKEY;
+		up_read(&keyring_key->sem);
+		goto out;
+	}
+>>>>>>> fca9d9623b16... UPSTREAM: ext4 crypto: add missing locking for keyring_key access
 	res = ext4_derive_key_aes(ctx.nonce, master_key->raw,
 				  raw_key);
+	up_read(&keyring_key->sem);
 	if (res)
 		goto out;
 got_key:
