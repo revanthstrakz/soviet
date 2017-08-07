285ed3ee0001d88e72629429d255086bacd694c6
RahifM <faizel326@gmail.com>
2017-08-08 01:08:06 +0530

    kernel: add build.sh
    Signed-off-by: RahifM <faizel326@gmail.com>

---
+++ build.sh
@@ -0,0 +1,11 @@
+#!/bin/sh
+export CROSS_COMPILE="/home/rahif/android/kernels/toolchains/aarch64-linux-android-4.9/bin/aarch64-linux-android-"
+
+export ARCH=arm64
+
+make clean O=out/
+make mrproper O=out/
+
+make mido_defconfig O=out/
+
+make -j4 O=out/
