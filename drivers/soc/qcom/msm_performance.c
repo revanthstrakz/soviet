a383ba1e3d164df234fc29e0d7864f82dd2f8fad
NATO66613 <normandija1945@gmail.com>
2017-09-16 22:02:35 +0000

    Disbale lot of debug,enable TCP configs, fix compile
    Conflicts:
    	arch/arm64/configs/mido_defconfig
    	drivers/platform/msm/ipa/ipa_common

--- drivers/soc/qcom/msm_performance.c
+++ drivers/soc/qcom/msm_performance.c
@@ -29,7 +29,7 @@
 static unsigned int use_input_evts_with_hi_slvt_detect;
 
 static struct mutex managed_cpus_lock;
-
+static int touchboost = 1;
 
 /* Maximum number to clusters that this module will manage*/
 static unsigned int num_clusters;
@@ -403,10 +403,6 @@ static int set_cpu_min_freq(const char *buf, const struct kernel_param *kp)
 	struct cpufreq_policy policy;
 	cpumask_var_t limit_mask;
 	int ret;
-	
-	if (touchboost == 0)
-		return 0;
-
 	const char *reset = "0:0 2:0";
 
 	if (touchboost == 0)
@@ -501,9 +497,6 @@ static int set_cpu_max_freq(const char *buf, const struct kernel_param *kp)
 	cpumask_var_t limit_mask;
 	int ret;
 
-	if (touchboost == 0)
-		return 0;
-	
 	while ((cp = strpbrk(cp + 1, " :")))
 		ntokens++;
 
@@ -2798,3 +2791,4 @@ static int __init msm_performance_init(void)
 	return 0;
 }
 late_initcall(msm_performance_init);
+
