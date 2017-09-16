fc9ac4068d8b50428d895defb5e9611439205aed
NATO66613 <normandija1945@gmail.com>
2017-09-16 03:53:54 +0000

    Revert "cpufreq: Add cpu utilization statistics to aid decisions made by userspace."
    This reverts commit 5725a6761ed279c3543345170939b73613f0ed99.

--- include/linux/cpufreq.h
+++ include/linux/cpufreq.h
@@ -77,7 +77,6 @@ struct cpufreq_policy {
 	unsigned int		max;    /* in kHz */
 	unsigned int		cur;    /* in kHz, only needed if cpufreq
 					 * governors are used */
-	unsigned int        util; 		  /* CPU utilization at max frequency */
 	unsigned int		restore_freq; /* = policy->cur before transition */
 	unsigned int		suspend_freq; /* freq to set during suspend */
 
@@ -454,10 +453,6 @@ static inline unsigned long cpufreq_scale(unsigned long old, u_int div,
  */
 #define CPUFREQ_POLICY_POWERSAVE	(1)
 #define CPUFREQ_POLICY_PERFORMANCE	(2)
-
-/* Minimum frequency cutoff to notify the userspace about cpu utilization
- * changes */
-#define MIN_CPU_UTIL_NOTIFY   40
 
 /* Governor Events */
 #define CPUFREQ_GOV_START	1
